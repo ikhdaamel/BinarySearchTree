82b40f5ba6996ea82c1c8411d090de94c402367d
ikhdaamel <ikhda.new45@gmail.com>
2023-07-01 18:39:05 +0700

    void search

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -55,4 +55,19 @@ public:
 			parent->rightchild = newNode;						//make the right child of the parent point to the newnode
 		}
 	}
+
+	void search(string element, Node* parent, Node*& currentNode)
+	{
+		//this function searches the currentNode of the specified Node as well as the current node of its parent
+		currentNode = ROOT;
+		parent = NULL;
+		while ((currentNode != NULL) && (currentNode->info != element))
+		{
+			parent = currentNode;
+			if (element < currentNode->info)
+				currentNode = currentNode->leftchild;
+			else
+				currentNode = currentNode->rightchild;
+		}
+	}
 };
